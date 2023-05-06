7faa887c7073f617d43cb99dcefaf91214211323
NatArys <91931609+NatArys@users.noreply.github.com>
2023-05-06 21:00:24 +0700

    feat: add task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+function copyDir(dir, dirCopy) {
+
+  fs.rm(path.join(__dirname, dirCopy), { force: true, recursive: true })
+    .then(() => {
+
+      fs.mkdir(path.join(__dirname, dirCopy), { recursive: true })
+        .then(() =>
+          fs.readdir(path.join(__dirname, dir), { withFileTypes: true })
+            .then(filenames => {
+              filenames.forEach(fname => {
+                if (fname.isFile())
+                  fs.copyFile(path.join(__dirname, dir, fname.name), path.join(__dirname, dirCopy, fname.name))
+              });
+            })
+        )
+
+    })
+
+}
+
+copyDir('files', 'files-copy');
