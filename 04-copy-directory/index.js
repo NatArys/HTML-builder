8342a4f68ca1e579bcf4d1f42d0923a119e995fb
NatArys <91931609+NatArys@users.noreply.github.com>
2023-05-08 22:07:23 +0700

    refactor: change foreach

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,18 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 function copyDir(dir, dirCopy) {
 
-  fs.rm(path.join(__dirname, dirCopy), { force: true, recursive: true })
+  fs.promises.rm(path.join(__dirname, dirCopy), { force: true, recursive: true })
     .then(() => {
 
-      fs.mkdir(path.join(__dirname, dirCopy), { recursive: true })
+      fs.promises.mkdir(path.join(__dirname, dirCopy), { recursive: true })
         .then(() =>
-          fs.readdir(path.join(__dirname, dir), { withFileTypes: true })
+          fs.promises.readdir(path.join(__dirname, dir), { withFileTypes: true })
             .then(filenames => {
               filenames.forEach(fname => {
                 if (fname.isFile())
-                  fs.copyFile(path.join(__dirname, dir, fname.name), path.join(__dirname, dirCopy, fname.name))
+                  fs.promises.copyFile(path.join(__dirname, dir, fname.name), path.join(__dirname, dirCopy, fname.name))
               });
             })
         )
